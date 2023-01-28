8c1252b56a69f49ede837953690d1b1c1aee7c86
XanatosX <10531466+XanatosX@users.noreply.github.com>
2023-01-28 14:30:31 +0100

    Add missing command descriptions

--- src/IL2CareerToolset/Commands/Cli/Web/OpenRepositoryCommand.cs
+++ src/IL2CareerToolset/Commands/Cli/Web/OpenRepositoryCommand.cs
@@ -1,7 +1,10 @@
 ﻿using IL2CareerToolset.Services;
 using Microsoft.Extensions.Configuration;
+using System.ComponentModel;
 
 namespace IL2CareerToolset.Commands.Cli.Web;
+
+[Description("Open the link to the public repository of the project")]
 internal class OpenRepositoryCommand : BaseOpenRepositoryLinkCommand
 {
     public OpenRepositoryCommand(IConfiguration configuration, OSInteractionService oSInteractionService) : base(configuration, oSInteractionService)
